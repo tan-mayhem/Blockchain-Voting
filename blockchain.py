c9e5da3701ac2024ef09834c19c47f74c62f691f
ssaraff98 <ssaraff@purdue.edu>
2018-11-10 16:14:10 -0500

    next block

--- blockchain.py
+++ blockchain.py
@@ -5,7 +5,6 @@ class Block():
     def __init__(self, index, timeStamp, voterId, voterPassword, vote, previousHash):
         self.index = index
         self.timeStamp = timeStamp
-        self.dayStamp = dayStamp
         self.voterId = voterId
         self.voterPassword = voterPassword
         self.vote = vote
@@ -14,13 +13,33 @@ class Block():
 
     def getCurrentHash(self):
         sha = hashlib.sha256()
-        sha.update = (str(self.index) + str(self.timeStamp) + str(self.dayStamp) + str(self.voterId) + str(self.voterPassword) + str(self.vote) + str(self.previousHash))
+        sha.update = (str(self.index) + str(self.timeStamp) + str(self.voterId) + str(self.voterPassword) + str(self.vote) + str(self.previousHash))
         return sha.hexdigest()
 
-def createGenesisBlock():
-    voterId = str(input("Enter your Voter ID: "))
-    voterPassword = str(input("Enter your password: "))
-    time.sleep(5)
-    print()
-    vote = str(input("Enter your vote: "))
-    return Block(0, date.datetime.now(), voterId, voterPassword, vote, "0")
+    def createGenesisBlock():
+        voterId = str(input("Enter your Voter ID: "))
+        voterPassword = str(input("Enter your password: "))
+        time.sleep(5)
+        print("Candidates for the House")
+        vote = candidateVote()
+        return Block(0, date.datetime.now(), voterId, voterPassword, vote, "0")
+
+    def next_block(last_block):
+		voterId = str(input("Enter your Voter ID "))
+		voterPassword = str(input("Enter your password: "))
+		time.sleep(5)
+		print("Candidates for the House")
+        vote = candidateVote()
+		this_index = last_block.index + 1
+		this_timestamp = date.datetime.now()
+		previousHash = last_block.currentHash
+		return Block(this_index, this_timestamp, voterId, voterPassword, vote, previousHash)
+
+    def candidateVote():
+        print("Index " + " Name ")
+        print("1. " + " Person 1 ")
+        print("2. " + " Person 2 ")
+        print("3. " + " Person 3 ")
+        print("4. " + " Person 4 ")
+        print("5. " + " Person 5 ")
+        return(str(input("Enter your vote: ")))
