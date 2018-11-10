9b74c546e834817e8f3cba69a241fe90ceaec474
Arindam Das <dasarindam98@gmail.com>
2018-11-10 15:05:37 -0600

    Added next_block function/block

--- blockvoter.py
+++ blockvoter.py
@@ -2,8 +2,15 @@ import hashlib
 
 class Block:
 	def next_block(last_block):
+		voterId = str(input("Enter your Voter ID "))
+		voterPassword = str(input("Enter your password: "))
+		time.sleep(5)
+		print()
+		vote = str(input("Enter your vote: "))
 		this_index = last_block.index + 1
 		this_timestamp = date.datetime.now() 
-		this_data = "Voter Number : " + str(this_index) 
-		this_hash = last_block.hash
-		return Block(this_index,this_timestamp, this_data, this_hash)
+		#this_data = "Voter Number : " + str(this_index) 
+		previousHash = last_block.currentHash
+		return Block(this_index,this_timestamp, voterId, voterPassword, vote, previousHash)
+
+#voterid voterPassword vote previousHash
