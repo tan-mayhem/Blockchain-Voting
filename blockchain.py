6baff99ee87a61c2c06e8ed39d2b866bcccca42d
Tanmay Bagaria <tanmaybagaria@Tanmays-MacBook-Pro.local>
2018-11-10 18:35:37 -0600

    User Authentication

--- blockchain.py
+++ blockchain.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python
+import base64
+import getpass
+def create():
+    username = "0"
+    file = open("userpass.txt","a")
+    while not username == "-1":
+        print("If you do not want to continue, enter -1")
+        username = str(raw_input("Enter your username : ").strip())
+        if(username == "-1"):
+            break
+        else:
+            password = getpass.getpass().strip()
+            cr = base64.encodestring(password)
+            file.write(username+","+cr+'\n')
+    file.close()
 
-my_dict = dict()
-username = "0"
-while not username == "-1":
-    print("If you do not want to continue, enter -1")
-    username = str(raw_input("Enter your username : "))
-    if(username == "-1"):
-        break
-    password = str(raw_input("Enter password: "))
 
-    my_dict[username] = password
-
-up_username = str(raw_input("Username: "))
-up_password = str(raw_input("Password: "))
-
-login = 0
-check = "-1"
-for key in my_dict:
-    if(key == up_username):
-        login = 1
-        check = my_dict.get(key)
-
-if(login == 0):
-    print("Try again")
-if(login == 1):
-    if(check == up_password):
-        print("Login successful")
+def enter():
+    up_username = str(raw_input("Username: ").strip())
+    up_password = password = getpass.getpass().strip()
+    enc_pass = base64.encodestring(up_password)
+    with open("userpass.txt","r") as f:
+        for line in f:
+            if line.split(",")[0] == up_username and line.split(",")[1] == enc_pass:
+                return True
+        return False
+choice = str(raw_input("Add new usernames/passwords or login --> Enter [1 or 2]"))
+if choice=="1":  
+    create()
+else:
+    if enter():
+        print("Logged in!")
     else:
-        print("Try again")
-
-
-
-        
-
-
+        print("Incorrect!")
