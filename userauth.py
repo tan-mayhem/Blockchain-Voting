ff7566d951636dfaa658edf7baddac0d59f5e1f7
14coruma <14coruma@gmail.com>
2018-11-11 14:06:20 +0000

    * update troubled files

--- userauth.py
+++ userauth.py
@@ -1,58 +1,64 @@
 #!/usr/bin/python
 import base64
 import getpass
+from firebase_admin import db
+import pyrebase
+
+"""
 def create():
-    username = "0"
-    file = open("userpass.txt","a")
-    while not username == "-1":
-        print("If you do not want to continue, enter -1")
-        username = str(raw_input("Enter your username : ").strip())
-        if(username == "-1"):
+
+    config = {
+      'apiKey': 'AIzaSyBXQCdlBopceWbLcXV5lMAvoB1zoO8aOmg',
+      'authDomain': 'blockchain-voting-79bf6.firebaseapp.com',
+      'databaseURL': 'https://blockchain-voting-79bf6.firebaseio.com',
+      'storageBucket': 'blockchain-voting-79bf6.appspot.com',
+      'serviceAccount': 'blockchain-voting-79bf6-firebase-adminsdk-c8jef-ce8ee634ad.json'
+    }
+
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+    ref = db.reference()
+
+    voters_ref = ref.child('voters')
+    voters_ref.push({
+        'voter1': {
+            'voter ID': '123',
+            'password': '123'
+        },
+        'voter2': {
+            'voter ID': '456',
+            'password': '456'
+        }
+    })
+"""
+
+def enter(voterId, voterPassword):
+    encodedPassword = base64.encodestring(voterPassword)
+
+    config = {
+      'apiKey': 'AIzaSyBXQCdlBopceWbLcXV5lMAvoB1zoO8aOmg',
+      'authDomain': 'blockchain-voting-79bf6.firebaseapp.com',
+      'databaseURL': 'https://blockchain-voting-79bf6.firebaseio.com',
+      'storageBucket': 'blockchain-voting-79bf6.appspot.com',
+      'serviceAccount': 'blockchain-voting-79bf6-firebase-adminsdk-c8jef-ce8ee634ad.json'
+    }
+
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+
+    all_voters = db.child("voters").get()
+    for voter in all_voters.each():
+        vId = voter.val()['voterid']
+        print vId
+        vPs = voter.val()['password']
+        print vPs
+        if voterId == vId and voterPassword == vPs:
+            k = 1
             break
         else:
-            password = getpass.getpass().strip()
-            cr = base64.encodestring(password)
-            file.write(username+","+cr+'\n')
-    file.close()
+            k = 0
 
-
-def enter():
-    up_username = str(raw_input("Username: ").strip())
-    up_password = password = getpass.getpass().strip()
-    enc_pass = base64.encodestring(up_password)
-    with open("userpass.txt","r") as f:
-        for line in f:
-            if line.split(",")[0] == up_username and line.split(",")[1] == enc_pass:
-                return True
-        return False
-
-def verifyUser(username, password, bc):
-    # Verify count
-    count = 0
-    for i in range(len(bc)):
-        if bc[i][1] == username:
-            if count >= 1:
-                return False
-            else:
-                count += 1
-
-    # Verify uid and password
-    enc_pass = base64.encodestring(password)
-    with open("userpass.txt","r") as f:
-        for line in f:
-            if line.split(",")[0] == username and line.split(",")[1] == enc_pass:
-                return True
-        return False
-
-def main():
-    choice = str(raw_input("Add new usernames/passwords or login --> Enter [1 or 2]"))
-    if choice=="1":  
-        create()
+    if k == 1:
+        return True
     else:
-        if enter():
-            print("Logged in!")
-        else:
-            print("Incorrect!")
-
-if __name__ == "__main__":
-    main()
+        return False
