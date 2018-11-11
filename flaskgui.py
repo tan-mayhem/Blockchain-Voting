422e52174f0d9f8c8bb84dd4dc378eb0ded71fa9
ssaraff98 <ssaraff@purdue.edu>
2018-11-11 08:54:45 -0500

    gui changes

---
+++ flaskgui.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, url_for, request
+app = Flask(__name__)
+"""
+# Route for handling the login page logic
+@app.route('/gui', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        result = request.form
+    return render_template('gui.html', error=error)
+
+if __name__ == '__main__':
+   app.run(debug = True)
+"""
+
+@app.route('/gui', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        result = request.form
+    return render_template('gui.html')  # render a template
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
