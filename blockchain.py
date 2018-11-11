5118082a3b7dfad5f83ff6040da8a81f053f1a59
14coruma <14coruma@gmail.com>
2018-11-11 02:30:21 +0000

    * voter: use blockchain

--- blockchain.py
+++ blockchain.py
@@ -18,6 +18,9 @@ class Block:
         sha = hashlib.sha256(str(self.index) + str(self.timeStamp) + str(self.voterId) + str(self.voterPassword) + str(self.vote) + str(self.previousHash))
         return sha.hexdigest()
 
+    def block_to_array(self):
+        return (self.index, self.voterId, self.voterPassword, self.vote, self.previousHash, self.currentHash)
+
 def createGenesisBlock():
     voterId = raw_input("Enter your Voter ID: ")
     voterPassword = raw_input("Enter your password: ")
@@ -49,16 +52,34 @@ def next_block(last_block):
     previousHash = last_block.currentHash
     return Block(this_index, this_timestamp, voterId, voterPassword, vote, previousHash)
 
-blockchain = []
-blockchain.append(createGenesisBlock())
-previous_block = blockchain[0]
+def next_block_from_array(last_block):
+    voterId = raw_input("Enter your Voter ID: ")
+    voterPassword = raw_input("Enter your password: ")
+    time.sleep(1)
+    print "Candidates for the House"
+    print "Index " + " Name "
+    print "1. " + " Person 1 "
+    print "2. " + " Person 2 "
+    print "3. " + " Person 3 "
+    print "4. " + " Person 4 "
+    print "5. " + " Person 5 "
+    vote = raw_input("Enter your vote: ")
+    this_index = last_block[0] + 1
+    this_timestamp = date.datetime.now()
+    previousHash = last_block[5]
+    return Block(this_index, this_timestamp, voterId, voterPassword, vote, previousHash)
 
-number_blocks = 20
-
-for i in range(0, number_blocks):
-  block_to_add = next_block(previous_block)
-  blockchain.append(block_to_add)
-  previous_block = block_to_add
-  print "Block #{} has been added to the blockchain!".format(block_to_add.index)
-  print "Hash: {}\n".format(block_to_add.currentHash)
-  print blockchain
+#blockchain = []
+#blockchain.append(createGenesisBlock())
+#print(blockchain[0].block_to_array())
+#previous_block = blockchain[0]
+#
+#number_blocks = 20
+#
+#for i in range(0, number_blocks):
+#  block_to_add = next_block(previous_block)
+#  blockchain.append(block_to_add)
+#  previous_block = block_to_add
+#  print "Block #{} has been added to the blockchain!".format(block_to_add.index)
+#  print "Hash: {}\n".format(block_to_add.currentHash)
+#  print blockchain
--- voter.py
+++ voter.py
@@ -8,26 +8,34 @@ import random
 import sys
 import json
 
-class NodeList():
-    myNodes = list()
+from blockchain import Block, createGenesisBlock, next_block_from_array
 
-    def add_node(self, host, port):
-        self.myNodes.append([host, port])
-
-    def set_nodes(self, nodes):
-        self.myNodes = nodes
+bc = list()
+nodes = list()
 
 class ConnectionHandler(SocketServer.BaseRequestHandler):
     def handle(self):
+        # Add connected host and port to list of clients
         text = self.request.recv(1024).strip()
         text_obj = json.loads(text)
-        nodes = self.server.nodes
-        nodes.add_node(text_obj["host"], text_obj["port"])
-        json_obj = {}
-        json_obj["nodes"] = nodes.myNodes
-        self.request.sendall(json.dumps(json_obj))
 
-def start_server(host, port, nodes):
+        nodes = self.server.nodes
+        bc = self.server.bc
+        if text_obj["type"] == "get_blockchain":
+            nodes.append((text_obj["host"], text_obj["port"]))
+            json_obj = {}
+            json_obj["nodes"] = nodes
+            print(nodes)
+            json_obj["blockchain"] = bc
+            print(bc)
+            self.request.sendall(json.dumps(json_obj))
+        elif text_obj["type"] == "add_vote":
+            nodes = text_obj["nodes"][:]
+            # TODO: approve vote
+            bc.append(text_obj["vote"])
+            print(bc)
+
+def start_server(host, port):
     """
     start (single threaded) server - receive connections
     """
@@ -36,21 +44,43 @@ def start_server(host, port, nodes):
         ConnectionHandler,
     )
     server.nodes = nodes
+    server.bc = bc
     server.serve_forever()
 
-def start_client(myHost, myPort, destHost, destPort, nodes):
+def send_blockchain_to_network(b, nodes):
+    print(nodes)
+    for i in range(0, len(nodes)-1):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((nodes[i][0], nodes[i][1]))
+
+        data = { "type": "add_vote", "vote": b, "nodes": nodes }
+        s.sendall(json.dumps(data))
+        s.close()
+
+def start_client(myHost, myPort, destHost, destPort):
     """
     start client - create connections
     """
+    # Make connection
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((destHost, destPort))
-    data = { "host": myHost, "port": myPort }
+
+    # Send my hostname and port
+    data = { "type": "get_blockchain", "host": myHost, "port": myPort }
     s.sendall(json.dumps(data))
+
+    # Receive list of nodes in network and blockchain
     r = s.recv(1024)
     r_data = json.loads(r)
-    nodes.set_nodes(r_data.get("nodes"))
-    print(nodes.myNodes)
+    nodes = r_data.get("nodes")[:]
+    bc = (r_data.get("blockchain"))
+    print(bc)
+    b = next_block_from_array(bc[-1]).block_to_array()
+    bc.append(b)
+
     s.close()
+    
+    send_blockchain_to_network(b, nodes)
 
 def main():
     host = "localhost"
@@ -61,21 +91,23 @@ def main():
         port = int(raw_input("\tMy port: "))
 
     print("Serving {}:{}".format(host, port))
-
-    nodes = NodeList()
+    nodes.append((host, port))
 
     server_t = threading.Thread(
-        target=start_server, args=(host, port, nodes)
+        target=start_server, args=(host, port)
     )
     server_t.daemon = True
     server_t.start()
 
     genesis = str(raw_input("Are you genesis? [y/N] ")) == "y"
-    if not genesis:
+    if genesis:
+        b = createGenesisBlock()
+        bc.append(b.block_to_array())
+    else:
         dHost = raw_input("Other host: ")
         dPort = int(raw_input("Other port: "))
         client_t = threading.Thread(
-            target=start_client, args=(host, port, dHost, dPort, nodes),
+            target=start_client, args=(host, port, dHost, dPort),
         )
         client_t.daemon = True
         client_t.start()
