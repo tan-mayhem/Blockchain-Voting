873666a60a8328b26a6cc5fe192146d28e5947ae
ssaraff98 <ssaraff@purdue.edu>
2018-11-11 07:08:15 -0500

    gui changes

--- userauth.py
+++ userauth.py
@@ -4,6 +4,34 @@ import getpass
 from firebase_admin import db
 import pyrebase
 
+"""
+def create():
+
+    config = {
+      'apiKey': 'AIzaSyBXQCdlBopceWbLcXV5lMAvoB1zoO8aOmg',
+      'authDomain': 'blockchain-voting-79bf6.firebaseapp.com',
+      'databaseURL': 'https://blockchain-voting-79bf6.firebaseio.com',
+      'storageBucket': 'blockchain-voting-79bf6.appspot.com',
+      'serviceAccount': 'blockchain-voting-79bf6-firebase-adminsdk-c8jef-ce8ee634ad.json'
+    }
+
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+    ref = db.reference()
+
+    voters_ref = ref.child('voters')
+    voters_ref.push({
+        'voter1': {
+            'voter ID': '123',
+            'password': '123'
+        },
+        'voter2': {
+            'voter ID': '456',
+            'password': '456'
+        }
+    })
+"""
+
 def enter(voterId, voterPassword):
     encodedPassword = base64.encodestring(voterPassword)
 
@@ -21,8 +49,16 @@ def enter(voterId, voterPassword):
     all_voters = db.child("voters").get()
     for voter in all_voters.each():
         vId = voter.val()['voterid']
+        print vId
         vPs = voter.val()['password']
-        if voterId == vId and voterPassword == encodedvPs:
-            return True
+        print vPs
+        if voterId == vId and voterPassword == vPs:
+            k = 1
+            break
         else:
-            return False
+            k = 0
+
+    if k == 1:
+        return True
+    else:
+        return False
